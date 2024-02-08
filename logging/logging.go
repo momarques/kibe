ec282a6cce5ad66b33ef56d523fff35bf982cb12
momarques <michaeloliveira.mom32@gmail.com>
2024-02-08 12:02:58 +0100

    add logging config

---
+++ logging/logging.go
@@ -0,0 +1,18 @@
+package logging
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var Log = logrus.New()
+var LogFile = "debug.log"
+
+func init() {
+	file, err := os.OpenFile(LogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	Log.Out = file
+}
--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/momarques/kb/kubecontext"
+	"github.com/momarques/kb/logging"
 )
 
 func main() {
-	m := kubecontext.NewContextModel()
+	m, err := kubecontext.NewContextModel()
+	if err != nil {
+		fmt.Printf("failed to create model: %s", err)
+		os.Exit(1)
+	}
 	program := tea.NewProgram(m)
+
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile(logging.LogFile, "debug")
+		if err != nil {
+			fmt.Println("failed to set log file:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	if _, err := program.Run(); err != nil {
-		panic(err)
+		fmt.Println("failed to run program:", err)
+		os.Exit(1)
 	}
 }
